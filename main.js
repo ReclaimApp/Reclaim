e83e86eae92d6f96caaa6d57222314700b1a3142
austinkelsay <austinkelsay@yahoo.com>
2021-02-01 08:14:57 -0600

    Got basic electron window rendering with 'hello world'!

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("hello world")
+const {BrowserWindow, app} = require("electron")
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        backgroundColor: 'purple',
+        webPreferences: {
+            nodeIntegration: false,
+            worldSafeExecuteJavaScript: true,
+            contextIsolation: true
+        }
+    })
+
+    win.loadFile('index.html');
+}
+
+
+app.whenReady().then(createWindow)
