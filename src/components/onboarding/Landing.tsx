70cb0ba6798c2039870fda1eb99ec5e38ba3db35
austinkelsay <austinkelsay@yahoo.com>
2021-02-09 09:36:27 -0600

    Finshed basic landing page, now I need to hook up the local data to be put into state

--- src/components/onboarding/Landing.tsx
+++ src/components/onboarding/Landing.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import { Button, Container, Jumbotron } from 'react-bootstrap';
+import { Button, Container, Jumbotron, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Styled from 'styled-components';
 import StyleSheet from './onboarding.module.css';
 // import categoryScroll from '../../media/category-scroll.mp4';
+
+const StyledFbButton = Styled.button`
+  width: 25%;
+  margin: 3% auto;
+  padding: 1%;
+  font-size: 1.3rem;
+  background: royalBlue;
+  color: #fff;
+  border: 1px solid oldlace;
+  border-radius: 6px;
+`;
 
 const StyledButton = Styled.button`
   width: 25%;
   margin: 3% auto;
   padding: 1%;
   font-size: 1.3rem;
-  background: royalBlue;
+  background-color: #4b3f72;
   color: #fff;
   border: 1px solid oldlace;
   border-radius: 6px;
@@ -28,7 +39,13 @@ const Landing = () => {
           </p>
         </Container>
       </Jumbotron>
-      <Button as={StyledButton}>Log into Facebook</Button>
+      <Button as={StyledFbButton}>Log into Facebook</Button>
+      <Container>
+        <Row className={StyleSheet.row}>
+          <Button as={StyledButton}>Explore Facebook</Button>
+          <Button as={StyledButton}>Explore Twitter</Button>
+        </Row>
+      </Container>
     </div>
   );
 };
