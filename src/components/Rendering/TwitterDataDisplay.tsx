283990484cc092aeb1a7980135ea65594ef11b21
austinkelsay <austinkelsay@yahoo.com>
2021-02-22 08:56:20 -0600

    Got basic test implementation working with iframe

--- src/components/Rendering/TwitterDataDisplay.tsx
+++ src/components/Rendering/TwitterDataDisplay.tsx
@@ -5,7 +5,9 @@ import Placeholder from '../../placeholder.html';
 
 const TwitterDataDisplay = () => {
   const index = useSelector((state) => state.TwitterReducer.index);
-  return Placeholder;
+  return parse(
+    '<iframe src="./user_data/twitter-2021-01-15-5e0215d1c9ddaa58003e43c74b90ce6d60009eb09558fcd716a55a03df5aa46c/Your archive.html" name="twitter-data" style={width: "100vw", height: "100vh"} />'
+  );
 };
 
 export default TwitterDataDisplay;
