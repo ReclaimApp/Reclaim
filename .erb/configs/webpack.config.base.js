a315d47a0ee092a9ed6a0c10823ecdb04b539cf0
austinkelsay <austinkelsay@yahoo.com>
2021-02-24 11:19:46 -0600

    Made button columns as independant containers

--- .erb/configs/webpack.config.base.js
+++ .erb/configs/webpack.config.base.js
@@ -4,6 +4,7 @@
 
 import path from 'path';
 import webpack from 'webpack';
+const CircularDependencyPlugin = require('circular-dependency-plugin')
 import { dependencies as externals } from '../../src/package.json';
 
 export default {
@@ -42,5 +43,18 @@ export default {
     new webpack.EnvironmentPlugin({
       NODE_ENV: 'production',
     }),
-  ],
+    new CircularDependencyPlugin({
+      // exclude detection of files based on a RegExp
+      exclude: /a\.js|node_modules/,
+      // include specific files based on a RegExp
+      include: /dir/,
+      // add errors to webpack instead of warnings
+      failOnError: true,
+      // allow import cycles that include an asyncronous import,
+      // e.g. via import(/* webpackMode: "weak" */ './file.js')
+      allowAsyncCycles: false,
+      // set the current working directory for displaying module paths
+      cwd: process.cwd(),
+    })
+  ]
 };
