1a304d16363ea63ab209881db12ff363a88d756f
royeradames <royeraadames@gmail.com>
2021-03-02 01:37:51 -0500

    📦donwload to user_data, unzip file, clear all intervals

--- src/automation/facebook/scripts/downloadFile.tsx
+++ src/automation/facebook/scripts/downloadFile.tsx
@@ -1,28 +1,15 @@
 require('dotenv').config();
 import reattachFrame from './reattachFrame';
 import startReportDownloadStatus from './startReportDownloadStatus';
+import dataDir from "./dataDir"
+import extract from "extract-zip"
 
-async function downloadFile(page) {
+async function downloadFile(page, browser, browserNotCloseCorrectly) {
   /* select child frame */
   // have to do this because after waiting for file to be ready by realoding the page the
   // old doc is detach from the page
   // like if it was a Interval ID
   const doc = await reattachFrame(page);
-
-  /* setup console download reports and rename download file to recommended name */
-
-  // download file with defaul name and no duplicates
-  // custom file loader reports
-  // design for downloading 1 complete file at a time
-  let intervalID;
-
-  page.on('download', async (download) => {
-    /* wait for download file */
-    // notice when the download starts
-    console.log('going to start waiting for file');
-    // start download reports
-    intervalID = startReportDownloadStatus(download);
-  });
 
   /* Go to download option */
   // go to available copies to download the data
@@ -52,13 +39,60 @@ async function downloadFile(page) {
     ]);
   }
 
-  // save the download file has the suggested file name
+  /* setup console download reports and rename download file to recommended name */
+  // download file with defaul name and no duplicates
+  // custom file loader reports
+  // design for downloading 1 complete file at a time
+  let intervalID = [];
+
+  page.on('download', async (download) => {
+    /* wait for download file */
+    // notice when the download starts
+    console.log('going to start waiting for file');
+    // start download reports
+    intervalID.push(startReportDownloadStatus(download));
+  });
+
+  /* if the browser is close while the reports are going then close the reports */
+  browser.on('disconnected', () => {
+    // stop download report interval
+    intervalID.forEach(id => {
+      clearInterval(id);
+    })
+  })
+
+  /* if the page times out say so and close the browser */
+  page.on('requestfailed', async (request) => {
+    console.log("the request has failed")
+    // start a new request by going to the request url
+    console.log("Going to start a new request")
+    console.log("---This can break the script---")
+    doc.goto(await request.url())
+    console.log("Finish starting a new request")
+  })
+
+  /* save the download file has the suggested file name */
   // last report
+  try {
+    console.log ("check if the donwload has failed: " + await download.failure())
+  } catch (error) {
+    console.log("failure does not work")
+  }
+
   const fileName = await download.suggestedFilename();
-  await download.saveAs(`./data/${fileName}`);
+  const fileNamePath = `${dataDir("facebook")}/${fileName}`
+  const facebookFile = await download.saveAs(fileNamePath);
+
+  try {
+    console.log("timing: " + await download.timing());
+  } catch (error) {
+    console.log("timing didn't work")
+  }
 
   // stop download report interval
-  clearInterval(intervalID);
+  intervalID.forEach(id => {
+    clearInterval(id);
+  })
 
   // report that downlolad is finished
   console.log(
@@ -68,6 +102,27 @@ async function downloadFile(page) {
   // delete the criptic file name
   await download.delete();
 
+  // unzip the folder
+  try {
+    await extract(fileNamePath, { dir: dataDir })
+    console.log('Extraction complete')
+  } catch (err) {
+    // could not unzip
+    console.log("Could not handle unzipping")
+    console.log(err)
+  }
+
+  // delete the zip file
+  try {
+    console.log("Going to delete zip file")
+    console.log(await facebookFile)
+    await facebookFile.delete()
+
+  } catch (error) {
+    console.log("Could not delete zip file")
+    console.log(error)
+
+  }
   /* close browser */
   console.log('finished download');
 }
