6d1f745b480cfd09c70d2a7b46046d1e84e8c53e
royeradames <royeraadames@gmail.com>
2021-03-01 20:01:09 -0500

    👌if you get an error while login in catch it and close the browser

--- src/playWright/scripts/storeCredentials.tsx
+++ src/playWright/scripts/storeCredentials.tsx
@@ -14,31 +14,41 @@ async function login() {
     headless: false,
     downloadsPath: 'D:\\Lambda\\projects\\puppeteer_test\\data',
   });
-  // Create a new incognito browser context.
-  const context = await browser.newContext({
-    acceptDownloads: true,
-    viewport: null,
-  });
-  // Create a new page in a pristine context.
-  const page = await context.newPage();
 
-  /* Authentication */
-  await page.goto(
-    'https://www.facebook.com/dyi/?x=AdkadZSUMBkpk0EF&referrer=yfi_settings'
-  );
+  try {
+    // Create a new incognito browser context.
+    const context = await browser.newContext({
+      acceptDownloads: true,
+      viewport: null,
+    });
+    // Create a new page in a pristine context.
+    const page = await context.newPage();
 
-  // Interact with login form
-  await page.fill('#email', process.env.ID || "No Id");
-  await page.fill('#pass', process.env.PASS || "No password");
-  await page.click('[type=submit]');
-  // wait for login
-  await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+    /* Authentication */
+    await page.goto(
+      'https://www.facebook.com/dyi/?x=AdkadZSUMBkpk0EF&referrer=yfi_settings'
+    );
 
-  // Save storage state and store as an env variable
-  const storage = await context.storageState();
-  process.env.STORAGE = JSON.stringify(storage);
+    // Interact with login form
+    await page.fill('#email', process.env.ID || "No Id");
+    await page.fill('#pass', process.env.PASS || "No password");
+    await page.click('[type=submit]');
+    // wait for login
+    await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
 
-  // close headfull browser
-  await browser.close();
+    // Save storage state and store as an env variable
+    const storage = await context.storageState();
+    process.env.STORAGE = JSON.stringify(storage);
+
+    // close headfull browser
+    await browser.close();
+  } catch (error) {
+    console.log("Something when wrong when login in")
+    console.log(error)
+
+    // close headfull browser
+    await browser.close();
+  }
+
 }
 export default login;
