8acd92c892bf20350135fe44c0c904b30d7d2639
austinkelsay <austinkelsay@yahoo.com>
2021-03-04 15:41:41 -0600

    Twitter is now working with the refactor

--- src/components/Rendering/TwitterDataDisplay.tsx
+++ src/components/Rendering/TwitterDataDisplay.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import parse from 'html-react-parser';
 import { useSelector } from 'react-redux';
-import Placeholder from '../../placeholder.html';
 
 const TwitterDataDisplay = () => {
+  const documentsPath = window.process.argv.slice(-1)[0];
   const name = useSelector((state) => state.TwitterReducer.folderName);
   return parse(
-    `<iframe src="./user_data/${name}/Your archive.html" name="twitter-data" style="width: 100vw; height: 100vh; background: white;" />`
+    `<iframe src="${documentsPath}/${name}/Your archive.html" name="twitter-data" style="width: 100vw; height: 100vh; background: white;" />`
   );
 };
 
