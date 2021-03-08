ad5108be4ff28d5b3d761871a167fee4b55fa509
royeradames <royeraadames@gmail.com>
2021-03-08 14:21:39 -0500

    👌change name to writeDocument so it does not conflict with method name

--- src/automation/helperFunctions/writeFile.js
+++ src/automation/helperFunctions/writeFile.js
@@ -1,10 +1,10 @@
-import fs from "fs"
+import {writeFile} from "fs"
 
-const writeFile = ( filePathName, data ) => {
-  fs.writeFile(filePathName, data, function (err) {
+const writeDocument = async ( filePathName, data ) => {
+  await writeFile(filePathName, data, function (err) {
     if (err) return console.log(err);
     console.log(`File Created: ${filePathName}`);
   });
 }
 
-export default writeFile
+export default writeDocument
