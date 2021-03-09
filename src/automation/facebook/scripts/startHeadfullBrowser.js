b5ce2d116876af96d53d5294f73023a05db80c90
royeradames <royeraadames@gmail.com>
2021-03-09 01:48:20 -0500

    📦start headful browser for user to interact with

---
+++ src/automation/facebook/scripts/startHeadfullBrowser.js
@@ -0,0 +1,24 @@
+import { chromium } from 'playwright';
+
+const startHeadfullBrowser = async () => {
+    const browser = await chromium.launch({
+    args: [
+      '--start-maximized',
+      '--disable-notifications',
+      '--disable-extensions',
+      '--mute-audio',
+    ],
+    defaultViewport: null,
+    headless: false,
+  });
+  // Create a new incognito browser context.
+    const context = await browser.newContext({
+      viewport: null,
+    });
+    // Create a new page in a pristine context.
+    const page = await context.newPage();
+
+    return [browser, page]
+  }
+
+  export default startHeadfullBrowser
