9ba5b7647f25c4f82067140112b715e07759f0a7
royeradames <royeraadames@gmail.com>
2021-03-10 00:53:20 -0500

    👌handle the credentials

--- src/automation/facebook/scripts/setUpBrowser.tsx
+++ src/automation/facebook/scripts/setUpBrowser.tsx
@@ -1,28 +1,41 @@
 require('dotenv').config();
 import { chromium } from 'playwright';
+import getCredentials from "./getCredentials"
 
-async function setUpBrower(storageState, downloadPath) {
+async function setUpBrower(downloadPath) {
+  /* get credentials */
+  // get user account access
+  const storageState = await getCredentials()
+  console.log({storageState})
 
-  /* start headless browser with credentials */
-  const browser = await chromium.launch({
-    args: [
-      '--start-maximized',
-      '--disable-notifications',
-      '--disable-extensions',
-      '--mute-audio',
-    ],
-    defaultViewport: null,
-    devtools: true,
-    slowMo: 100,
-    downloadsPath: downloadPath,
+  const isThereCrendtialsFile = storageState === null ? false : true
 
-  });
-  // Create a new incognito browser context with user credentials
-  const context = await browser.newContext({
-    acceptDownloads: true,
-    viewport: null,
-    storageState: JSON.parse(storageState),
-  });
-  return [browser, context];
+  if(isThereCrendtialsFile){
+    // run the scrip if there is login file/cookies
+    /* start headless browser with credentials */
+    const browser = await chromium.launch({
+      args: [
+        '--start-maximized',
+        '--disable-notifications',
+        '--disable-extensions',
+        '--mute-audio',
+      ],
+      defaultViewport: null,
+      devtools: true,
+      slowMo: 100,
+      downloadsPath: downloadPath,
+
+    });
+
+    // Create a new incognito browser context with user credentials
+    const context = await browser.newContext({
+      acceptDownloads: true,
+      viewport: null,
+      storageState: JSON.parse(storageState),
+    });
+
+    return {browser, context, isRunScript: true};
+  }
+  else return {isRunScript: false}
 }
 export default setUpBrower;
