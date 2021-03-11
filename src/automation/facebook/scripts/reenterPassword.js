16dc8a78c5f5c5ec54db21d93968e5f14bd598ab
royeradames <royeraadames@gmail.com>
2021-03-11 15:55:31 -0500

    👌console log in what step the script is on

--- src/automation/facebook/scripts/reenterPassword.js
+++ src/automation/facebook/scripts/reenterPassword.js
@@ -1,6 +1,7 @@
 import startHeadfullBrowser from "./startHeadfullBrowser"
 import gotoDownloadOption from "./gotoDownloadOption"
 const reenterPassword = async (props) => {
+  console.log("starting the reenterpassword manual process")
   const {pageUrl, absoluteCredentialsPath} = props
   /* solution if I where to have their password
       // retype password
@@ -17,17 +18,31 @@ const reenterPassword = async (props) => {
   /* let user enter their password */
   const isRunWithCredentials = true
   const {browser, page} = await startHeadfullBrowser({isRunWithCredentials, absoluteCredentialsPath})
+  debugger
 
-  await page.goto(pageUrl)
+  // go to the download page
+  await page.goto(pageUrl, {waitUntil: "networkidle"})
+  console.log("Go to the download page")
+  debugger
+
+  // focus on the frame and go to the download tab
+  console.log("Heading to the download tab")
   let doc = await gotoDownloadOption(page)
+  console.log("on the download tab")
+
+  // click the submit button
+  console.log("going to click the download button")
   await doc.click("button[type=submit]")
+  console.log("finishing clicking the download button")
 
   // wait until the password fill is gone, and the file start downloading
   //todo: needs to be tested
+  console.log("Wait for the user to enter password")
   await Promise.all([
     doc.waitForSelector('td button[type=submit]', {state:'detached', timeout: 0}),
     page.waitForEvent('download', {timeout: 0})
   ])
+  console.log("Finishing waiting for user to insert the password")
 
   // close the browser
   browser.close()
